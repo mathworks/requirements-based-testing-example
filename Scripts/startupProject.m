d0af88a768b701663af6d19269d73e4417022aaa
patcanny <patcanny@mathworks.com>
2019-06-25 17:39:56 -0400

    minor update to startup script

--- Scripts/startupProject.m
+++ Scripts/startupProject.m
@@ -9,30 +9,14 @@ function startupProject
     addpath(genpath(fullfile(pRoot, 'Models')));
     addpath(fullfile(pRoot, 'Tests'));
     addpath(fullfile(pRoot, 'Scripts'));
-    addpath(fullfile(pRoot, 'Scripts', 'WindowFcns'));
-    addpath(fullfile(pRoot, 'Scripts', 'SLDV'));
 
-
-    %% Set location of slprj to be "Work" folder of current project:
-    workFolder = fullfile(pRoot, 'Work');
-    if ~exist(workFolder, 'dir')
-        mkdir(workFolder);
-    end
-
-    % Add work folder to path
-    addpath(workFolder);
-
-    % Change into work folder 
-    cd(workFolder);
-
-    % clear workspace
+     % clear workspace
     clear
     
     % clear command window
     clc
 
     %% open instructions
-    %openInstructions
     edit Instructions.mlx
 
 end
