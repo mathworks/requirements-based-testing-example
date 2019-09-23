92a87b9edd1123d56ead0e6a2d47e8250c3bd07a
patcanny <patcanny@ah-patcanny.dhcp.mathworks.com>
2019-09-23 10:30:52 -0400

    addressed CFE comments

--- Scripts/shutdownProject.m
+++ Scripts/shutdownProject.m
@@ -6,6 +6,8 @@ Simulink.fileGenControl('reset');
 
 %% Reset Simulation Data Inspector naming rule
 Simulink.sdi.setRunNamingRule('Run <run_index>: <model_name>');
-%%
+%% Clean things up
 clear
 clc
+%% Re-enable warnings
+warning('on','all')
--- Scripts/startupProject.m
+++ Scripts/startupProject.m
@@ -2,7 +2,8 @@ function startupProject
     clear
     % clear command window
     clc
-
+    % turn off all warnings
+    warning('off','all')
     %% open instructions
     edit Instructions.mlx
 
