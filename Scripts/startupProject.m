a6684d8e9cadeb26800be21b9d03fce1f06b2021
patcanny <patcanny@ah-patcanny.dhcp.mathworks.com>
2019-09-27 11:32:08 -0400

    added a GIF to show user how to widen Instructions.mlx window

--- Scripts/startupProject.m
+++ Scripts/startupProject.m
@@ -4,7 +4,16 @@ function startupProject
     clc
     % turn off all warnings
     warning('off','all')
-    %% open instructions
+    
+    %% Open instructions
     edit Instructions.mlx
 
+    %% Open GIF to show user how to widen Instructions.mlx
+    file_name = 'Widen_Instructions_Window.gif';
+    if ispc
+        winopen(file_name);
+    else
+        system(['open ', file_name]); 
+    end
+    clear file_name
 end
