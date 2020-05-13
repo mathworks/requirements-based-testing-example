d46a7e2003a9855fa3f19d57e4c67306fea4b9f5
Pablo Romero <promero@mathworks.com>
2020-05-13 10:36:40 +0200

    Don't open instructions if running as batch job.

--- Scripts/openInstructions.m
+++ Scripts/openInstructions.m
@@ -1,14 +1,16 @@
 function openInstructions
-    
+
+if ~batchStartupOptionUsed
     %% Open instructions
     edit Instructions.mlx
-
+    
     %% Open GIF to show user how to widen Instructions.mlx
     file_name = 'Widen_Instructions_Window.gif';
     if ispc
         winopen(file_name);
     else
-        system(['open ', file_name]); 
+        system(['open ', file_name]);
     end
-    
+end
+
 end
