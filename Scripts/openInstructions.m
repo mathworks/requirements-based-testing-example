64bd633d1511b34e8efefaa0bc38cfcfdce98974
patcanny <patcanny@ah-patcanny.dhcp.mathworks.com>
2021-03-22 20:53:14 -0400

    fixed startup and shutdown. Updated Instructions.

--- Scripts/openInstructions.m
+++ Scripts/openInstructions.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Scripts/startupProject.m
 function startupProject
     clear
     % clear command window
@@ -8,21 +7,12 @@ function startupProject
     
     %% Open instructions
     edit Instructions.mlx
-=======
 function openInstructions
 
 if ~batchStartupOptionUsed
     %% Open instructions
     edit Instructions.mlx
-    
-    %% Open GIF to show user how to widen Instructions.mlx
-    file_name = 'Widen_Instructions_Window.gif';
-    if ispc
-        winopen(file_name);
-    else
-        system(['open ', file_name]);
-    end
 end
 
->>>>>>> 32add79a5a4a41abc9152e32cd20c964f4604706:Scripts/openInstructions.m
+end
 end
--- Scripts/shutdownProject.m
+++ Scripts/shutdownProject.m
@@ -1,5 +1,4 @@
 function shutdownProject
-<<<<<<< HEAD
 % Clean up the environment for the current project.
 
 %% Reset location of slprj
@@ -12,8 +11,8 @@ clear
 clc
 %% Re-enable warnings
 warning('on','all')
-=======
+
 
 % Reset Simulation Data Inspector naming rule
 Simulink.sdi.setRunNamingRule('Run <run_index>: <model_name>');
->>>>>>> 32add79a5a4a41abc9152e32cd20c964f4604706
+end
